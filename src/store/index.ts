c81393d4329f09d0dc87f397bc83937d303c4733
jerrymusaga <musagatechnology@gmail.com>
2023-09-05 11:42:52 +0100

    set alert and load msg in global react hook

--- src/store/index.ts
+++ src/store/index.ts
@@ -7,6 +7,21 @@ const {setGlobalState, getGlobalState, useGlobalState} = createGlobalState({
     NFTDetailsModal: 'scale-0',
     reactionModal: 'scale-0',
     updatePriceModal: 'scale-0',
+    loading: {show: false, msg: ''},
+    alert: {show:false, msg:'', color:''},
 });
 
-export {setGlobalState, getGlobalState, useGlobalState}
+const setAlert = (msg: string,color='green') => {
+    setGlobalState('loading', {show:false,msg:''})
+    setGlobalState('alert', {show:true, msg, color})
+    setTimeout(() => {
+        setGlobalState('alert',{show:false, msg, color})
+    }, 2000)
+}
+
+const setLoadingMsg = (msg: string) => {
+    const loading = getGlobalState('loading')
+    setGlobalState('loading', {...loading, msg})
+}
+
+export {setGlobalState, getGlobalState, useGlobalState, setAlert, setLoadingMsg}
