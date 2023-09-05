72e28c7957bab96d34086db08fbe90cb7ac34771
jerrymusaga <musagatechnology@gmail.com>
2023-09-05 14:22:39 +0100

    adding web3modal to nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-
+'use client'
 import Nav from '@/components/Nav'
 import Hero from '@/components/Hero'
 import ArtWorks from '@/components/Artworks'
@@ -7,18 +7,39 @@ import NFTDetails from '@/components/NFTDetails'
 import ReactionsModal from '@/components/ReactionsModal'
 import UpdatePrice from '@/components/UpdatePrice'
 
+import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
+import { Web3Modal } from '@web3modal/react'
+import { configureChains, createConfig, WagmiConfig } from 'wagmi'
+import { gnosisChiado, gnosis } from 'wagmi/chains'
+
+
+const chains = [gnosisChiado, gnosis]
+const projectId = "eedf16a14abf9be796f6ee521c2c4191"
+
+const { publicClient } = configureChains(chains, [w3mProvider({ projectId })])
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors: w3mConnectors({ projectId, chains }),
+  publicClient
+})
+const ethereumClient = new EthereumClient(wagmiConfig, chains)
+
 export default function Home() {
   return (
+    
     <div className="min-h-screen">
-    <div className="gradient-bg-hero">
-      <Nav />
-      <Hero />
-    </div>
-    <ArtWorks />
-    <MintNFT />
-    <NFTDetails />
-    <ReactionsModal />
-    <UpdatePrice />
- </div>
+      <WagmiConfig config={wagmiConfig}>
+        <div className="gradient-bg-hero">
+          <Nav />
+          <Hero />
+        </div>
+        <ArtWorks />
+        <MintNFT />
+        <NFTDetails />
+        <ReactionsModal />
+        <UpdatePrice />
+      </WagmiConfig>
+      <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
+  </div>
   )
 }
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,11 +1,25 @@
+'use client';
+
 import * as React from 'react';
+import { Web3Button, useWeb3ModalTheme } from '@web3modal/react'
 
 const Nav = () => {
+
+  const { theme, setTheme } = useWeb3ModalTheme()
+
+  setTheme({
+    themeMode: 'dark',
+    themeVariables: {
+      '--w3m-font-family': 'Roboto, sans-serif',
+      '--w3m-accent-color': '#bd255f'
+      // ...
+    }
+  })
 
   return (
     <div className='w-4/5 flex justify-between md:justify-center items-center py-4 mx-auto'>
         <div className='md:flex-[0.5] flex-initial justify-center items-center text-gradient text-5xl font-bold'>
-            G-Arts
+          G-Arts
         </div>
         
         <ul className='md:flex-[0.5] text-white md:flex hidden list-none justify-between items-center flex-initial'  >
@@ -15,7 +29,9 @@ const Nav = () => {
             <li className='mx-4 cursor-pointer'>Profile</li>
             
         </ul>
-        <button  className='shadow-xl shadow-black text-white  hover:bg-[#bd255f] md:text-xl p-2 rounded-full' >Connect Wallet</button> 
+        
+          <Web3Button />  
+        
     </div>
   )
 }
